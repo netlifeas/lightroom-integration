71dff3026b4600909e233f77e0b054b23536799d
Atle Esperum <atle@esperum.no>
2017-06-09 13:25:21 +0200

    updated doc for manual install

--- src/main/resources/Config.lua
+++ src/main/resources/Config.lua
@@ -4,7 +4,7 @@ Config = {
     dbg = false, -- show debug message boxes
 	-- you can define rules  for wich portals this client should get based on portal prefix 
 	allowedCompanies = {}, -- example on company filter {"c1-","c2-","c3-"},
-	charsReservedToCompanyPrefix = 3, -- number of the first chars that is reserved to company(portal) prefix 
+	charsReservedToCompanyPrefix = 0, -- number of the first chars that is reserved to company(portal) prefix 
 	-- define company spesific rules that is validated against properties in the exportContext.propertyTable. If dbg = true all possible values are printed in the log file. 
 	companyExportSettings =
 		{
